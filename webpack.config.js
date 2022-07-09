feb33b84904b6dff79b47cf12f23585d5c82ee00
AntLer (Arch) <antler@inbox.ru>
2022-07-09 21:27:49 +0700

    feat: добавлены настройки окружения
    webpack: поддержка typescript,
    eslint: исправления по стилистике, проверил сразу весь проект пока не
    разросся
    prettier: также проверил весь проект

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const dotenv = require("dotenv");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
 
-dotenv.config()
+dotenv.config();
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.tsx',
+  target: 'web',
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
     port: process.env.PORT || 3000,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.css', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+        exclude: /(node_modules)/,
       },
       {
         test: /\.css$/,
@@ -36,18 +51,18 @@ module.exports = {
             loader: 'css-loader',
             options: {
               importLoaders: 1,
-            }
+            },
           },
           {
-            loader: 'postcss-loader'
-          }
-        ]
-      }
-    ]
+            loader: 'postcss-loader',
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    })
-  ]
+      template: './public/index.html',
+    }),
+  ],
 };
