182433aff73819d40d9dd8119d8e329a920dc51c
Vladislav <axperien@yandex.ru>
2022-07-12 11:58:40 +0400

    feat: добавлены формы к страницам входа и регистрации

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,9 +1,65 @@
-const SignIn = () => (
-  <div className="container mx-auto flex flex-row justify-center items-center flex-wrap h-full">
-    <h1 className="w-full mt-3 mb-3 text-3xl text-center font-bold">
-      Login page
-    </h1>
-  </div>
-);
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Link } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import { Form } from '../../components/Form';
+import { Input } from '../../components/Input';
+import { schema } from '../../utils/validate';
+
+type FormValues = {
+  login: string;
+  password: string;
+};
+
+const SignIn = () => {
+  const defaultValues: FormValues = {
+    login: '',
+    password: '',
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormValues>({
+    mode: 'onChange',
+    resolver: yupResolver(schema),
+    defaultValues,
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data: unknown) => {
+    if (isValid) {
+      // eslint-disable-next-line no-alert
+      alert(JSON.stringify(data));
+    }
+  };
+
+  return (
+    <div className="container mx-auto flex flex-row justify-center items-center flex-wrap h-full">
+      <Form title="Вход" handlerSubmit={handleSubmit(onSubmit)}>
+        <Input
+          placeholder="Логин"
+          {...register('login', { required: true })}
+          error={errors.login}
+        />
+        <Input
+          placeholder="Пароль"
+          type="password"
+          {...register('password', { required: true })}
+          error={errors.password}
+        />
+        <Button cls="w-full mt-12" text="Войти" type="submit" />
+        <div className="w-full text-center mt-3">
+          <Link
+            className="font-bold underline hover:no-underline"
+            to="/sign-up"
+          >
+            Нет аккаунта?
+          </Link>
+        </div>
+      </Form>
+    </div>
+  );
+};
 
 export default SignIn;
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,9 +1,97 @@
-const SignUp = () => (
-  <div className="container mx-auto flex flex-row justify-center items-center flex-wrap h-full">
-    <h1 className="w-full mt-3 mb-3 text-3xl text-center font-bold">
-      Register page
-    </h1>
-  </div>
-);
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Link } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import { Form } from '../../components/Form';
+import { Input } from '../../components/Input';
+import { schema } from '../../utils/validate';
+
+type FormValues = {
+  login: string;
+  password: string;
+  password_repeat: string;
+  email: string;
+  first_name: string;
+  second_name: string;
+  phone: string;
+};
+
+const SignUp = () => {
+  const defaultValues: FormValues = {
+    login: '',
+    password: '',
+    password_repeat: '',
+    email: '',
+    first_name: '',
+    second_name: '',
+    phone: '',
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormValues>({
+    mode: 'onChange',
+    resolver: yupResolver(schema),
+    defaultValues,
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data: unknown) => {
+    if (isValid) {
+      // eslint-disable-next-line no-alert
+      alert(JSON.stringify(data));
+    }
+  };
+
+  return (
+    <div className="container mx-auto flex flex-row justify-center items-center flex-wrap h-full">
+      <Form title="Регистрация" handlerSubmit={handleSubmit(onSubmit)}>
+        <Input
+          placeholder="Логин"
+          {...register('login', { required: true })}
+          error={errors.login}
+        />
+        <Input
+          placeholder="Имя"
+          {...register('first_name', { required: true })}
+          error={errors.first_name}
+        />
+        <Input
+          placeholder="Фамилия"
+          {...register('second_name', { required: true })}
+          error={errors.second_name}
+        />
+        <Input
+          placeholder="Телефон"
+          {...register('phone', { required: true })}
+          type="tel"
+          error={errors.phone}
+        />
+        <Input
+          placeholder="Пароль"
+          type="password"
+          {...register('password', { required: true })}
+          error={errors.password}
+        />
+        <Input
+          placeholder="Пароль еще раз"
+          type="password"
+          {...register('password_repeat', { required: true })}
+          error={errors.password_repeat}
+        />
+        <Button cls="w-full mt-12" text="Зарегистрироваться" type="submit" />
+        <div className="w-full text-center mt-3">
+          <Link
+            className="font-bold underline hover:no-underline"
+            to="/sign-in"
+          >
+            Войти
+          </Link>
+        </div>
+      </Form>
+    </div>
+  );
+};
 
 export default SignUp;
