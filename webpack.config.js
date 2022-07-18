fc98d611fea4ad90a19b67d727eae02914217e2c
Denis <dfgaraev@yandex.ru>
2022-07-18 18:54:50 +0300

    chore: добавлен eslint-plugin-prettier и source-map

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  devtool: 'source-map',
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'public'),
