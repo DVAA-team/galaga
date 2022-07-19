0de77bd215501a5440f846e0f5dabdc664ef72b2
Vladislav <axperien@yandex.ru>
2022-07-19 18:43:31 +0400

    feat: добавлена проверка авторизации при загрузке приложения

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,10 @@ import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { ToastContainer } from 'react-toastify';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../../store/slices/userSlice';
+import userService from '../../services/userService';
 import { Home } from '../../pages/Home';
 import { SignIn } from '../../pages/SignIn';
 import { SignUp } from '../../pages/SignUp';
@@ -14,20 +18,32 @@ import { GamePage } from '../../pages/Game';
 import { Leaderboard } from '../../pages/Leaderboard';
 import { NotFound } from '../../pages/NotFound';
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/leaderboard" element={<Leaderboard />} />
-      <Route path="/game" element={<GamePage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-    <ToastContainer />
-  </BrowserRouter>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    userService.getUser().then((user) => {
+      if (user !== null) {
+        dispatch(setUser({ user }));
+      }
+    });
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer />
+    </BrowserRouter>
+  );
+};
 
 export default App;
