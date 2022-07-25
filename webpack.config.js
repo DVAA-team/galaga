215332c41361e0d43c5391e049aaf5167eaaf821
Denis <dfgaraev@yandex.ru>
2022-07-25 09:40:55 +0300

    fix: в конфиг вебпака добавлен publicPath для корректной работы вложенных роутов

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
   },
   devtool: 'source-map',
   devServer: {
@@ -99,6 +100,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      inject: 'body',
     }),
     new SpriteLoaderPlugin(),
   ],
