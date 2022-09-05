cb3f04440eaa65acf5644e25476b834b731c71e4
Vladislav <axperien@yandex.ru>
2022-09-05 13:23:01 +0400

    test: исправлены тесты

--- src/pages/Dashboard/Dashboard.test.tsx
+++ src/pages/Dashboard/Dashboard.test.tsx
@@ -1,9 +1,24 @@
 import { render } from '@/utils/test-utils';
 import Dashboard from '@/pages/Dashboard/Dashboard';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import userReducer from '@/store/slices/userSlice';
+import { configureStore } from '@reduxjs/toolkit';
 
 describe('Страница дашборда', () => {
   test('должна корректно рендериться', () => {
-    const { getByText } = render(<Dashboard />);
+    const mockStore = configureStore({
+      reducer: {
+        user: userReducer,
+      },
+    });
+    const { getByText } = render(
+      <Provider store={mockStore}>
+        <Router>
+          <Dashboard />
+        </Router>
+      </Provider>
+    );
 
     expect(getByText('Dashboard page')).toBeInTheDocument();
   });
--- src/pages/Leaderboard/Leaderboard.test.tsx
+++ src/pages/Leaderboard/Leaderboard.test.tsx
@@ -1,10 +1,25 @@
 import { render } from '@/utils/test-utils';
 import Leaderboard from '@/pages/Leaderboard/Leaderboard';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import userReducer from '@/store/slices/userSlice';
+import { configureStore } from '@reduxjs/toolkit';
 
 describe('Страница лидерборда', () => {
   test('должна корректно рендериться', () => {
-    const { getByText } = render(<Leaderboard />);
+    const mockStore = configureStore({
+      reducer: {
+        user: userReducer,
+      },
+    });
+    const { getByText } = render(
+      <Provider store={mockStore}>
+        <Router>
+          <Leaderboard />
+        </Router>
+      </Provider>
+    );
 
-    expect(getByText('Leaderboard page')).toBeInTheDocument();
+    expect(getByText('Лучшие игроки')).toBeInTheDocument();
   });
 });
