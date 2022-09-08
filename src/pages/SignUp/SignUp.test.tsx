0ca28fa60be9d7a053366729d01c0f8103baf42d
Anton Satyshev <satushev@gmail.com>
2022-09-08 20:01:28 +0700

    test: поправлены тесты
    заменил старый способ задания стора на новый

--- src/pages/SignUp/SignUp.test.tsx
+++ src/pages/SignUp/SignUp.test.tsx
@@ -1,17 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '@/store/slices/userSlice';
 import { render } from '@/utils/test-utils';
 import { Provider } from 'react-redux';
 import { MemoryRouter as Router } from 'react-router-dom';
 import SignUp from '@/pages/SignUp/SignUp';
+import { initialStore } from '@/store';
 
 describe('Страница регистрации', () => {
   test('должна корректно рендериться', () => {
-    const mockStore = configureStore({
-      reducer: {
-        user: userReducer,
-      },
-    });
+    const mockStore = initialStore();
     const { getByText } = render(
       <Provider store={mockStore}>
         <Router>
