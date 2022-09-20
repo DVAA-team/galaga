9067b2679f3a93f60c8f6c475692a61510ccdd58
Bayusov <woland.rtf@gmail.com>
2022-09-20 22:10:57 +0600

    ci: update yandex deploy

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source .env &&
-docker rm -f $(docker ps -a --filter name=galaga --format "{{.ID}}") &&
+docker rm -f "$(docker ps -a --filter name=galaga --format "{{.ID}}")" || true &&
 docker run --rm=false -d --name galaga -p 3000:$PORT \
 -e DATABASE_URL=$DATABASE_URL \
 cr.yandex/$1/$2:$3
